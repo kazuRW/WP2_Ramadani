fb44eb26be4cbf1fb724391bf0e935c9d8c1c683
ramadani <ramzdanigan@gmail.com>
2021-10-24 20:26:43 +0700

    mengerjakan model_contoh2

--- controllers/latihan1.php
+++ controllers/latihan1.php
@@ -9,7 +9,7 @@ class latihan1 extends CI_Controller
 
     public function penjumlahan($n1, $n2)
     {
-        $this->load->model('model_contoh1');
+        $this->load->model('model_contoh2');
         $hasil = $this->model_contoh2->jumlah($n1, $n2);
         echo "hasil penjumlahan dari ". $n1 ." + ". $n2 . " = "
 .$hasil;
